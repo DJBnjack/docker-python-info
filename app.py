a1fa4a7abadea6469e848ba0658ebca8a627f4b8
Johannes Bertens <johannes.bertens@live.nl>
2016-08-15 19:39:46 +0200

    added personal message

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from flask import Flask
 import os, sys
 
-if len(sys.argv) == 2:
+if len(sys.argv) >= 2:
     port = int(sys.argv[1])
 else:
     port = 5000
+
+if len(sys.argv) >= 3:
+    message = sys.argv[2]
+else:
+    message = "world"
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    ret = "<h1>Hello, world!</h1>"
+    ret = "<h1>Hello, " + message + "!</h1>"
     ret += "<h3>This is my environment:</h3>"
     ret += "<table>"
     ret += "<tr><th>Key</th><th>Value</th></tr>"
